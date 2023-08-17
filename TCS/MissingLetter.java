b667c3ee1e39408f46c5c6ac64f807b4c9aad41f
Vasu-Unique <vasanth2k472@gmail.com>
2023-08-17 23:43:08 +0530

    saved

--- TCS/MissingLetter.java
+++ TCS/MissingLetter.java
@@ -1,23 +1,25 @@
+package TCS;
 import java.util.*;
 public class MissingLetter {
     public static void main(String args[])
     {
         Scanner obj=new Scanner(System.in);
-        String a=obj.nextLine().toUpperCase();
-        String b=obj.nextLine().toUpperCase();
+        String a=obj.nextLine();
+        String b=obj.nextLine();
         int count=0;
         int i;
-        for( i=0;i!=a.length();i++)
+        for( i=0;i<a.length();i++)
         {
             if (a.charAt(i) != b.charAt(i)) {
+                System.out.print(b.charAt(i));
+            if(a.charAt(i)==b.charAt('\0'))
+            {
                 System.out.print(a.charAt(i));
-               // break;
+            }
         }
-        //System.out.println(count);
+        
         }
-        if (i == a.length()) {
-            System.out.print("NA");
-        }
+        
          
     }
 }
