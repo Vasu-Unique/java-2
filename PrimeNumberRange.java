c9c56178f168e5a539569366226c0044f0d92d0c
Vasu-Unique <vasanth2k472@gmail.com>
2024-01-31 14:44:51 +0530

    31-01-24

--- PrimeNumberRange.java
+++ PrimeNumberRange.java
@@ -1,4 +1,34 @@
 import java.util.*;
+public class PrimeNumberRange
+{
+   public static void main(String args[])
+   {
+      Scanner obj=new Scanner(System.in);
+      int n=obj.nextInt();
+      if(n==1)
+      {
+         n++;
+      }
+         for(int i=2;i<=n;i++)
+         {
+            int flag=1;
+            for(int j=2;j<=Math.sqrt(i/2);j++)
+            {
+               if(i%j==0)
+               {
+                  flag=0;
+                  break;
+               }
+            }
+            if(flag==1)
+            {
+               System.out.print(i+" ");
+            }
+         }
+      
+   }
+}
+
 // public class PrimeNumberRange{
 //    public static void main ( String args[])
 //    {
@@ -30,35 +60,3 @@ import java.util.*;
 //    }
 //    }
 // }
-
-public class PrimeNumberRange{
-   public static void main(String[] args) {
-      Scanner obj=new Scanner(System.in);
-      int a=obj.nextInt();
-      int b=obj.nextInt();
-
-      if(a==1)
-      {
-         a++;
-      }
-      else
-      {
-         for(int i=a;i<=b;i++)
-         {
-            boolean flag=true;
-            for(int j=2;j<=Math.abs(i/2);j++)
-            {
-               if(i%j==0)
-               {
-                  flag=false;
-                  break;
-               }
-            }
-            if(flag)
-            {
-               System.out.print(i+" ");
-            }
-         }
-      }
-}
-}
--- primeNu.java
+++ primeNu.java
@@ -1,55 +1,33 @@
 import java.util.*;
-// public class primeNu
-// {
-//     public static void main(String args[])
-//     {
-//         Scanner obj=new Scanner(System.in);
-//         int n=obj.nextInt();
-//         int flag=1;
-
-//         if(n==1||n==0)
-//         {
-//             System.out.println("NOT");
-//                flag=0;
-//         }   
-//         else{
-//             for(int i=2;i<=n/2;i++)  //Math.sqrt(n) use pannalam
-//             {
-//                 if(n%i==0)
-//                 {
-//                     System.out.println("NOT");
-//                     flag=0;
-//                     break;
-//                 }
-//             }
-//         }
-//         if(flag==1)
-//         {
-//             System.out.println("prime");
-//         }
-//     }
-// }
-
-class primeNu{
-    public static void main(String[] args) {
-        Scanner obj =new Scanner (System.in);
+public class primeNu
+{
+    public static void main(String args[])
+    {
+        Scanner obj=new Scanner(System.in);
         int n=obj.nextInt();
+        int flag=1;
 
-        for(int i=0;i<=n/2;i++)
+        if(n==1||n==0)
         {
-            if(n%i==0)
+            System.out.println("NOT");
+               flag=0;
+        }   
+        else{
+            for(int i=2;i<=n/2;i++)  //Math.sqrt(n) use pannalam
             {
-                System.out.println("prime");
-                break;
+                if(n%i==0)
+                {
+                    System.out.println("NOT");
+                    flag=0;
+                    break;
+                }
             }
-            else
-            {
-                System.out.println("NOt");
-                break;
-            }
-                
         }
-
+        if(flag==1)
+        {
+            System.out.println("prime");
+        }
     }
 }
+
 
